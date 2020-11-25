476dcb3eb162df74203cb3f21a78603690b39dea
Nikita <trofimov1nv@gmail.com>
2020-11-25 21:21:07 +0300

    Added package manager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,13 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] wallSearch(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset, ) {
+        return null;
+    }
+    public void delete(int ownerId, int postId) {
+    }
+}
